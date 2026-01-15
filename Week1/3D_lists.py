6ab5f74b040fd2b6f0f663af3de7f05bb56042ef
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-01-16 00:12:13 +0530

    3D Lists

---
+++ Week1/3D_lists.py
@@ -0,0 +1,18 @@
+# 3D-Lists in python
+
+# A simple example
+cube = [
+    [  # Layer 0
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [  # Layer 1
+        [7, 8, 9],
+        [10, 11, 12]
+    ]
+]
+
+print(cube)
+# Output: [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+print(cube[0][1][2]) # Prints 6
+print(cube[1][0][0]) # Prints 7
