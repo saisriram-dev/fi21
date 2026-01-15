a1738c4f2e74ab3ef9cbe558ac5ae994d0344973
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-01-16 00:15:44 +0530

    Added looping concept in 3D-Lists

--- Week1/3D_lists.py
+++ Week1/3D_lists.py
@@ -16,3 +16,10 @@ print(cube)
 # Output: [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
 print(cube[0][1][2]) # Prints 6
 print(cube[1][0][0]) # Prints 7
+
+# Looping through the 3D List
+for layer in cube:
+    for row in layer:
+        for value in row:
+            print(value, end=" ")
+    print()
