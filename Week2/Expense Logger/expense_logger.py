177dda6c577e3eb945a8b19a212fdae5be648f65
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-01-19 00:00:20 +0530

    A meaningful update in expense logger program

--- Week2/Expense Logger/expense_logger.py
+++ Week2/Expense Logger/expense_logger.py
@@ -1,8 +1,20 @@
-expense = int(input("Enter your today's total expense: "))
 file_path = "C:/Users/P SAI SRI RAM/OneDrive/Desktop/fi21/Week2/Expense Logger/expenses.txt"
-with open(file_path, "a") as file:
-    file.write(f"\n{expense}")
+
+""" Initial program """
+# expense = int(input("Enter your today's total expense: "))
+# with open(file_path, "a") as file:
+#     file.write(f"\n{expense}")
+
+# with open(file_path, "r") as file:
+#     content = file.read()
+#     print(content)
 
 with open(file_path, "r") as file:
-    content = file.read()
-    print(content)
+    lines = [line.rstrip("\n") for line in file.readlines()] # file.readlines() gives a list as output
+    lines = [int(line) for line in lines]
+    print(lines)
+    
+    total_expense = sum(lines)
+    average_expense = total_expense / len(lines)
+    print(f"Total expense: {total_expense}")
+    print(f"Average expense: {average_expense}")
