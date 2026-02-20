c231e40d191ff57ee7423a07bbd854f1ccf96cbb
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-02-20 23:52:13 +0530

    Add function to determine the state of a tic-tac-toe board

---
+++ Week4/experiment.py
@@ -0,0 +1,27 @@
+def is_solved(board):
+    lines = []
+
+    # rows
+    for i in range(3):
+        lines.append(board[i])
+
+    # columns
+    for j in range(3):
+        lines.append([board[i][j] for i in range(3)])
+
+    # diagonals
+    lines.append([board[i][i] for i in range(3)])
+    lines.append([board[i][2-i] for i in range(3)])
+
+    # check winner
+    for line in lines:
+        if line == [1, 1, 1]:
+            return 1
+        if line == [2, 2, 2]:
+            return 2
+
+    # check unfinished
+    if any(0 in row for row in board):
+        return -1
+
+    return 0
