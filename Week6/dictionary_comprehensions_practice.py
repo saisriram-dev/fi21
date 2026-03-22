af8b95f01bf00ecfd132bb4ec9c7195a9c3f6265
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-03-22 18:28:27 +0530

    Learned about comprehensions and solved some problems based on them

---
+++ Week6/dictionary_comprehensions_practice.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,4,5]
+squares = {num: num **2 for num in nums}
+print(squares)
+
+marks = {"ram": 45, "krishna": 82, "arjun": 67, "bheem": 90}
+filtered = {name: value for name, value in marks.items() if value >= 70}
+print(filtered)
+
+words = ["python", "AI", "code", "data"]
+lengths = {word: len(word) for word in words}
+print(lengths)
+
+d = {"a": 1, "b": 2, "c": 3}
+swapped = {v: k for k, v in d.items()}
+print(swapped)
+
+s = "banana"
+freq = {ch: s.count(ch) for ch in set(s)}
+print(freq)
---
+++ Week6/list_comprehensions_practice.py
@@ -0,0 +1,23 @@
+nums = [i for i in range(1, 11)]
+squares_even = [num ** 2 for num in nums if num%2 == 0]
+print(squares_even)
+
+words = ["hi", "hello", "world", "AI", "python"]
+res = [word for word in words if len(word) > 3]
+print(res)
+
+celsius = [0, 10, 20, 30, 40]
+fahrenheit = [(c * 1.8) + 32 for c in celsius]
+print(fahrenheit)
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened = [num for row in matrix for num in row]
+print(flattened)
+
+nums = [1, 2, 3, 4, 5, 6]
+label = ["even" if num%2 == 0 else "odd" for num in nums]
+print(label)
+
+nums = [1, 2, 3, 4]
+all_comb = [(x, y) for x in nums for y in nums]
+print(all_comb)
