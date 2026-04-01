51a30ceb3895226e3a4b724e6e353548ef5315a1
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-04-01 19:24:28 +0530

    Learnt about a few methods.

---
+++ Week6/class_method.py
@@ -0,0 +1,25 @@
+class Employee:
+    company = "TCS"
+
+    def __init__(self, name):
+        self.name = name
+
+    # This is a class method, it can be called on the class itself, it takes the class as the first argument
+    # It is used to change the class variable, it can also be used as an alternative constructor
+    @classmethod 
+    def change_company(cls, new_company):
+        cls.company = new_company
+
+e1 = Employee("Alice")
+e2 = Employee("Bob")
+
+print(e1.company) # TCS
+print(e2.company) # TCS
+
+# Even though we are calling the change_company method on the instance e1, 
+# it is actually changing the class variable company for all instances of the class Employee, 
+# because it is a class method and it takes the class as the first argument.
+e1.change_company("Google")
+
+print(e1.company) # Google
+print(e2.company) # Google
