51ab32fdc6a684a054531e0dbf3a253ae73288ab
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-04-09 18:52:01 +0530

    Added more functionality to cav_analyzer.py

--- Week7/csv_analyzer.py
+++ Week7/csv_analyzer.py
@@ -1,13 +1,47 @@
-import sys
 import pandas as pd
 
-def main():
-    file_path = sys.argv[1]
+file_path = input("Enter the path to the CSV file: ")
+df = pd.read_csv(file_path)
 
-    df = pd.read_csv(file_path)
+class CSVAnalyzer:
+    def __init__(self, df):
+        self.df = df
 
-    print("CSV loaded successfully")
-    print(df.head())
+    def analyze(self):
+        print(" A basic analysis of all the columns in the CSV file: ")
+        for column in self.df.columns:
+            print(f"Column: {column}")
+            print(f"Data Type: {self.df[column].dtype}")
+            print(f"Number of Unique Values: {self.df[column].nunique()}")
+            print(f"Number of Missing Values: {self.df[column].isnull().sum()}")
+            print("-" * 40)
+    
+    def insight(self):
+        for column in self.df.columns:
+            if self.df[column].dtype in ["int64", "float64"]:
+                print(f"\nInsights for the column: {column}")
+                print(self.df[column].describe())
+                print(f"The number of missing values is: {self.df[column].isnull().sum()}")
+                print(f"Number of duplicate values: {self.df[column].duplicated().sum()}")
+            elif self.df[column].dtype in ["object", "string"]:
+                print(f"\nInsights for the column: {column}")
+                print(self.df[column].describe())
+                missing_count = (
+                    self.df[column].isnull() |
+                    self.df[column].astype(str).str.strip().eq("")
+                    ).sum()
+                print(f"The number of missing values is: {missing_count}")
+                print(f"Number of duplicate values: {self.df[column].duplicated().sum()}")
+            else:
+                print(f"Insights for the column: {column}")
+                print(self.df[column].describe())
 
-if __name__ == "__main__":
-    main()
+insight_type = input("How do you want to analyze the csv data (analyze / insight)?: ")
+analyzer = CSVAnalyzer(df)
+
+if insight_type == "analyze":
+    analyzer.analyze()
+elif insight_type == "insight":
+    analyzer.insight()
+else:
+    print("Invalid input. Please enter 'analyze' or 'insight'.")
