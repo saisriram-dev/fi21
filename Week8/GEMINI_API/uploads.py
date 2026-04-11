9692cd951661564ad62a589b5565fd3f61a04fdc
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-04-11 23:36:55 +0530

    Created an uploads.py file which conatains the basics of uploading files and getting information regarding it using the Gemini API

---
+++ Week8/GEMINI_API/uploads.py
@@ -0,0 +1,21 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+# Load the API key from the .env file
+load_dotenv()
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Initialize the Gemini client with the API key
+client = genai.Client(api_key=API_KEY)
+
+# Upload a file to the Gemini API
+uploaded_file = client.files.upload("Week8\GEMINI_API\Screenshot (684).png")
+
+# Getting a response regarding the uploaded file
+res = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=["Describe the content of the uploaded file.", uploaded_file]
+)
+
+print(res.text)
