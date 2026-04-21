ed760751da255c8867ae360f14f6e92e07102c22
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-04-21 22:16:47 +0530

    Learned the use of the module tenacity and made a sample script describing it's use

---
+++ Week8/tenacity.py
@@ -0,0 +1,28 @@
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+# Retry function use
+# @retry is a decorator that retries the function if it fails
+@retry
+def risky_operation():
+    # Your code here
+    pass
+
+# Other parameters
+# stop_after_attempt: Number of attempts to stop after
+# wait_exponential: Wait time between attempts
+
+"""
+    Example with parameters:
+    The below code will retry the function 3 times with exponential backoff
+    The exponential backoff means the wait time will increase exponentially
+    In this case, the wait time will be 4, 8, 16 seconds
+    Multiplier is the factor by which the wait time will be multiplied
+    Min is the minimum wait time
+    Max is the maximum wait time
+    In this case the maximum wait time is 10 seconds
+    So the wait time sequence will be 4, 8, 10 (capped at max)
+"""
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
+def risky_operation():
+    # Your code here
+    pass
