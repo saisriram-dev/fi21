14c605b15b349e4669aa5fcab97315dcec8b1ea8
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-04-30 22:44:52 +0530

    Added eroor handling to the script

--- Week9/FastAPI/Frontend-Backend_Flow_Example/query.py
+++ Week9/FastAPI/Frontend-Backend_Flow_Example/query.py
@@ -18,13 +18,21 @@ app.add_middleware(
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+# Define the request model
+# If we get a JSON object with a "text" field, we can use this model
+# Eg. {"text": "Hello"} is Query(text="Hello") and we can access it as Query.text
 class Query(BaseModel):
     text: str
 
 @app.post("/query")
 def process_query(query: Query):
-    response = client.models.generate_content(
-        model="gemini-2.0",
-        contents=query.text
-    )
-    return {"result": response.text}
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=query.text
+        )
+
+        return {"result": response.text}
+
+    except Exception as e:
+        return {"Error": str(e)}
