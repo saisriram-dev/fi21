04b3bdbcecc4205ddae2123a2b55eb0b9258c5f5
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-05-01 09:38:17 +0530

    Added meaningful comments and improved the security by modifying origins in CORS Middleware

--- Week9/FastAPI/Frontend-Backend_Flow_Example/query.py
+++ Week9/FastAPI/Frontend-Backend_Flow_Example/query.py
@@ -9,9 +9,18 @@ load_dotenv()
 
 app = FastAPI()
 
+# List of allowed origins for CORS
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:8000"
+]
+
+# Add CORS middleware to allow requests from the frontend
+# This is required to allow the frontend to make requests to the backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
