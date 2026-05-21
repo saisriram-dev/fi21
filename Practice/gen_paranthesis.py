7d513b3102279cf2e4422892101849105f916728
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-05-21 23:11:07 +0530

    Practiced a leetcode problem to generate all sorts of combinations of parenthesis

---
+++ Practice/gen_paranthesis.py
@@ -0,0 +1,32 @@
+def generateParenthesis(n):
+    result = []
+
+    def backtrack(current, open_count, close_count):
+
+        # Base case
+        if len(current) == 2 * n:
+            result.append(current)
+            return
+
+        # Add opening bracket
+        if open_count < n:
+            backtrack(
+                current + "(",
+                open_count + 1,
+                close_count
+            )
+
+        # Add closing bracket
+        if close_count < open_count:
+            backtrack(
+                current + ")",
+                open_count,
+                close_count + 1
+            )
+
+    backtrack("", 0, 0)
+
+    return result
+
+
+print(generateParenthesis(3))
