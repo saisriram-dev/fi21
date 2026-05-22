4a453bd74ee2f87cc14687bf535413c5216cc612
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-05-22 23:53:05 +0530

    A leetcode problem solution which is used to calculate te maximum depth of a binary tree

---
+++ Practice/max_depth_tree.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def maxDepth(root):
+    if not root:
+        return 0
+
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+
+    return 1 + max(left_depth, right_depth)
