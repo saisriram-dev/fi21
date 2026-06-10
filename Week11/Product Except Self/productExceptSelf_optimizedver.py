9327e149d68f206ea975b6b0c2387fbd98ae60ba
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-06-10 13:28:57 +0530

    Solved the Leetcode problem - "Product Except Self"

---
+++ Week11/Product Except Self/productExceptSelf_optimizedver.py
@@ -0,0 +1,14 @@
+def productExceptSelf(nums):
+    res = []
+
+    left_product = 1
+    for i in range(len(nums)):
+        res[i] = left_product
+        left_product *= nums[i]
+    
+    right_product = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= right_product
+        right_product *= nums[i]
+    
+    return res
