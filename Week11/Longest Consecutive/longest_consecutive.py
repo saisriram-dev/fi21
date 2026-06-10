e54080391a22d9d97aed0333eda27d85fa061402
Sai Sri Ram Pitta <pssr0701@gmail.com>
2026-06-10 14:54:26 +0530

    Silved the Leetcode problem - "Longest Consecutive Sequence "

---
+++ Week11/Longest Consecutive/longest_consecutive.py
@@ -0,0 +1,17 @@
+def longestConsecutive(nums):
+    num_set = set(nums)
+    longest = 0
+
+    for num in num_set:
+        # Start of a sequence
+        if num - 1 not in num_set:
+            current = num
+            length = 1
+
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+
+            longest = max(longest, length)
+
+    return longest
